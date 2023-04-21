30feb3c2a264f76aa63fde6e13a0d619a7b19736
Paulo Joao Candrinho <paulojoaocandrinho4@gmail.com>
2023-04-21 14:01:59 +0200

    paulo commit 21/04

--- Controller/script-index-rotas.js
+++ Controller/script-index-rotas.js
@@ -1,9 +1,15 @@
 const a ={
-    btnEntrar: document.querySelector('#entrar-btn')
+    btnEntrar: document.querySelector('#entrar-btn'),
+    btnRegistar: document.querySelector('#register-page')
 }
 
 a.btnEntrar.addEventListener("click", entrar);
+a.btnRegistar.addEventListener("click", registar);
 
 function entrar(){
     window.location.href = "../View/pages/login.html";
+}
+
+function registar() {
+    window.location.href = "../View/pages/register.html";
 }
